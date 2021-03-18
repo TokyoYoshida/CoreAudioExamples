4b72469a5a53b52fd4e6f08d252cd581772bb2c4
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-03-18 15:30:46 +0900

    add vc

---
+++ CoreAudioExamples/Samples/GenerateWave/GenerateWaveViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GenerateWaveViewController.swift
+//  CoreAudioExamples
+//
+//  Created by TokyoYoshida on 2021/03/18.
+//
+
+import UIKit
+
+class GenerateWaveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
