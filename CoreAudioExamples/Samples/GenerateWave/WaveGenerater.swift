062c66e7737f23d384f7e86b9b2bcfc5213c6153
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-03-18 15:39:11 +0900

    add initialize func

--- CoreAudioExamples/Samples/GenerateWave/WaveGenerater.swift
+++ CoreAudioExamples/Samples/GenerateWave/WaveGenerater.swift
@@ -6,7 +6,23 @@
 //
 
 import Foundation
+import AVFoundation
 
 class WaveGenerater {
+    var audioUnit: AudioUnit?
     
+    func prepare() {
+        var acd = AudioComponentDescription()
+        acd.componentType = kAudioUnitType_Output
+        acd.componentSubType = kAudioUnitSubType_RemoteIO
+        acd.componentManufacturer = kAudioUnitManufacturer_Apple
+        acd.componentFlags = 0
+        acd.componentFlagsMask = 0
+        
+        let audioComponent = AudioComponentFindNext(nil, &acd)!
+        
+        AudioComponentInstanceNew(audioComponent, &audioUnit)
+
+        AudioUnitInitialize(audioUnit!)
+    }
 }
